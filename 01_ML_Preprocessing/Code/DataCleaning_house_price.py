2900a0934d5c555f1cf4d8a9462fd585a8906c12
hadolesuraj <suraj.hadole123@gmail.com>
2026-09-14 22:38:47 +0530

    Linear Regression

---
+++ 01_ML_Preprocessing/Code/DataCleaning_house_price.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+df= pd.read_excel("Datasets\\rawData\\fetch_california_housing.xlsx")
+#print(df.head)
+#print(df.info())
+#print(df.isnull().sum())
+print(df.corr())
+
+X = df.drop(columns= ["price"])
+y=df["price"]
+print(X)
+
+vif = pd.DataFrame()
+vif["Features"]=X.columns 
+vif["VIF"]= [variance_inflation_factor(X,i) for i in range(X.shape[1])]
+
+print(vif)
+#Deleting columns having  multi colinerarity
+X=X[['MedInc', 'HouseAge', 'AveRooms', 'Population', 'AveOccup','Longitude']]
+
+sclr = StandardScaler()
+
+X_sclr = sclr.fit_transform(X)
+print(X_sclr)
+
+print(X_sclr.shape)
+
+X_train,X_test,y_train,y_test=train_test_split(X_sclr,y,test_size=0.2,random_state=1)
+#Random state -1 is the pointer, to split the data and maintain the same pointer 
+
+print(X_train.shape,X_test.shape)
+
+modal = LinearRegression()
+modal.fit(X_train,y_train)
+
+print("Modal_coef",modal.coef_)
+print("modal intercept",modal.intercept_)
+
+y_pred = modal.predict(X_test)
+print(y_pred)
+print(y_test)
+
